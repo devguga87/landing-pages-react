e314d306831cb652a093d7e581394f6dd7df3997
Gustavo Seabra <devguga87@gmail.com>
2021-10-19 22:32:37 -0300

    feature: SectionBackground e SectionContainer

---
+++ src/components/SectionContainer/SectionContainer.test.jsx
@@ -0,0 +1,8 @@
+import { SectionContainer } from '.';
+import { renderTheme } from '../../utils/render-theme';
+
+describe('<SectionContainer/>', () => {
+  it('should render <SectionContainer />', () => {
+    renderTheme(<SectionContainer />);
+  });
+});
