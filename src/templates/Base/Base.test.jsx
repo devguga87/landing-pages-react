36928a97936d5e49eb0e7f5eabdd87e9b4444658
Gustavo Seabra <devguga87@gmail.com>
2021-10-22 10:19:39 -0300

    feature: BaseTemplate

---
+++ src/templates/Base/Base.test.jsx
@@ -0,0 +1,8 @@
+import { Base } from '.';
+import { renderTheme } from '../../utils/render-theme';
+
+describe('<Base/>', () => {
+  it('should render', () => {
+    renderTheme(<Base />);
+  });
+});
